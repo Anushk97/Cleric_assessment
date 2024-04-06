6bbb605ba0c4eb95ee21103ede7dfa208cc122da
Anushk <emmy@Anushks-MacBook-Pro.local>
2024-04-06 14:04:27 +0800

    made same changes in main as updates

--- app.py
+++ app.py
@@ -330,6 +330,10 @@ def reset_questions_and_facts():
 
     return jsonify({'status': 'success'})
 
+@app.route("/")
+def home():
+    return render_template('home.html')
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
 
